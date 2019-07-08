833fd018a08dd5ceb10a2cd6ce0b0ab320f42717
ayodelesilver <ayodelefibs@yahoo.com>
2019-07-08 19:46:21 +0100

    created forgot password test

---
+++ src/test/java/com/w2a/testcase/ForgotPasswordTest.java
@@ -0,0 +1,25 @@
+package com.w2a.testcase;
+
+
+
+	 import org.openqa.selenium.By;
+	   import org.testng.annotations.Test;
+
+	import com.w2a.base.TestBase;
+
+	public class ForgotPasswordTest extends TestBase  {
+	    	
+	    
+			@Test
+			
+			public void InvalidLogin () throws InterruptedException {
+				
+				click("HomeloginBtn_XPATH");
+				Thread.sleep(2000);
+				click("forgotpasswordBtn_XPATH");
+				driver.findElement(By.xpath(OR.getProperty("resetpasswordemail_XPATH"))).sendKeys(OR.getProperty("email_TEXT"));
+				click("retrieveSignUpBtn_XPATH");
+				
+}
+			
+}
--- src/test/java/com/w2a/testcase/InvalidLoginTest.java
+++ src/test/java/com/w2a/testcase/InvalidLoginTest.java
@@ -15,7 +15,7 @@ import com.w2a.base.TestBase;
 			
 			click("HomeloginBtn_XPATH");
 			Thread.sleep(2000);
-			driver.findElement(By.xpath(OR.getProperty("email_XPATH"))).sendKeys(OR.getProperty("invalidEmail_TEXT"));
+			driver.findElement(By.xpath(OR.getProperty("email_XPATH"))).sendKeys(OR.getProperty("email_TEXT"));
 			driver.findElement(By.name(OR.getProperty("password_NAME"))).sendKeys(OR.getProperty("password_TEXT"));		
 			click("signupBtn_ID");
 			Thread.sleep(3000);
