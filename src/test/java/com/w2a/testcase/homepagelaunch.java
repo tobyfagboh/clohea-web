56828f093bbefde2de11699681677979b290bfee
tolajide49 <tolajide49@yahoo.com>
2019-07-08 20:38:12 +0100

    Verified that homepage launched

---
+++ src/test/java/com/w2a/testcase/homepagelaunch.java
@@ -0,0 +1,17 @@
+package com.w2a.testcase;
+
+import org.openqa.selenium.By;
+
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+public class homepagelaunch extends TestBase {
+	
+	@Test
+	public void homepagelaunch () throws InterruptedException {
+	
+	// Validation of homepage log in
+		click("testsiteurl");
+		Thread.sleep(2000);
+}}
