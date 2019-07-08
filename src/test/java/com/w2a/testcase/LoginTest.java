f0f45bbd52e1ce5759ad6d318ddc7c31959abf08
tolajide49 <tolajide49@yahoo.com>
2019-07-08 16:42:25 +0100

    verification that signup and login buttons works

--- src/test/java/com/w2a/testcase/LoginTest.java
+++ src/test/java/com/w2a/testcase/LoginTest.java
@@ -11,15 +11,27 @@ public class LoginTest extends TestBase {
 	@Test
 	public void invalidLogin () throws InterruptedException {
 		
-		click("HomeloginBtn_XPATH");
-		Thread.sleep(2000);
-		driver.findElement(By.id(OR.getProperty("email_ID"))).sendKeys(OR.getProperty("invalidEmail_TEXT"));
-		driver.findElement(By.id(OR.getProperty("password_ID"))).sendKeys(OR.getProperty("password_TEXT"));		
-		click("loginBtn_XPATH");
-		Thread.sleep(3000);
-		Assert.assertTrue(isElementPresent(By.className(OR.getProperty("alert_CLASSNAME"))), "Your Credential is not Correct");
-		log.debug("Log in successfully executed");
-		
-	}
-
-}
+		// Lauching homepage
+				click("HomeloginBtn_XPATH");
+				Thread.sleep(2000);
+				// enter email address
+				driver.findElement(By.xpath(OR.getProperty("email_XPATH"))).sendKeys(OR.getProperty("email_TEXT"));
+				// enter password
+				driver.findElement(By.xpath(OR.getProperty("password_XPATH"))).sendKeys(OR.getProperty("password_TEXT"));	
+				// verify that sign in button works
+				click("loginBtn_XPATH");
+				
+			// sign out 
+				click("logoutdropdown_XPATH");
+				Thread.sleep(2000);
+				click("logoutbtn_XPATH");
+				Thread.sleep(9000);
+				
+				// Verify that sign up button works
+				click("Homesignupbtn_XPATH");
+				
+				Assert.assertTrue(isElementPresent(By.className(OR.getProperty("alert_CLASSNAME"))), "Your Credential is not Correct");
+				log.debug("Log in successfully executed");  
+				
+	}}
+				
