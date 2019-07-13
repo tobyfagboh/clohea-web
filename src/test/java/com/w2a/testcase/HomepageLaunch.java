ecfa5a8aa29f82e1f6c5db3e547525ef3818a154
tolajide49 <tolajide49@yahoo.com>
2019-07-13 22:40:15 +0100

    CA-4: Validate homepage Launch

--- src/test/java/com/w2a/testcase/HomepageLaunch.java
+++ src/test/java/com/w2a/testcase/HomepageLaunch.java
@@ -1,5 +1,15 @@
 package com.w2a.testcase;
+import org.testng.annotations.Test;
 
-public class HomepageLaunch {
+import com.w2a.base.TestBase;
 
-}
+public class HomepageLaunch extends TestBase  {
+	@Test
+	public void homepagelaunch () throws InterruptedException {
+	
+	// Validation of homepage log in
+		
+		click("testsiteurl");
+		Thread.sleep(2000);
+
+}}
